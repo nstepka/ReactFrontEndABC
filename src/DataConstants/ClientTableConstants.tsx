89cdd83a49eb97c15def434eca6f77896de9ffde
Briana Martinez <115733336+bgmartine@users.noreply.github.com>
2022-11-30 12:32:02 -0600

    Add files via upload

--- src/DataConstants/ClientTableConstants.tsx
+++ src/DataConstants/ClientTableConstants.tsx
@@ -1,15 +1,9 @@
-
-
-import { ClientTableRow } from '../DataObjects/ClientTableInterface';
+import {ClientTableRow} from '../DataObjects/ClientTableInterface';
 
 export const INIT_RESULT_DATA: ClientTableRow = {
-    abc_client_id: -1,
-    client_name: "",
-    phone_number: "",
-    company_address_id: undefined,
-    created_by: undefined,
-    created_date: undefined,
-    modified_by: undefined,
-    modified_date: undefined,
-    is_deleted: undefined
+  id: -1,
+  ClientName: "",
+  AddressState: "",
+  InventoryCount: 0,
+  ContactCount: 0
 };
